a044b380aedb1d8335c20ec5a70b0e672c87d231
Caio Feitosa <68925245+caiofeitosa7@users.noreply.github.com>
2023-02-15 11:01:29 -0300

    Adaptando a procura pelas variáveis
    Variáveis agora estão guardadas em sets. Foi feita mudança no acesso a elas.

--- utils.py
+++ utils.py
@@ -16,18 +16,23 @@ def lanca_excecao(texto):
 
 
 def visivel_no_escopo():
-    nomes_funcoes = [funcao.nome for funcao in memoria_global['funcoes']]
-    uso_liberado = memoria_global['constantes'] + memoria_global['variaveis'] + nomes_funcoes
-
-    if funcao_executando:
-    	funcao_atual = busca_funcao_por_nome(funcao_executando)
-    	uso_liberado += funcao_atual.variaveis
-
-    return uso_liberado
+	uso_liberado = set()
+	
+	if funcao_executando:
+		funcao_atual = busca_funcao_por_nome(funcao_executando)
+		uso_liberado = set(list(funcao_atual.variaveis.keys()))
+	
+	nomes_funcoes = [funcao.nome for funcao in memoria_global['funcoes']]
+	escopo_global = memoria_global['constantes'] + list(memoria_global['variaveis'].keys()) + nomes_funcoes
+	
+	for identificador in escopo_global:
+		uso_liberado.add(identificador)
+	
+	return uso_liberado
 
 
 def busca_funcao_por_nome(nome_funcao):
-    return [f for f in memoria_global['funcoes'] if f.nome == nome_funcao][0]
+	return [f for f in memoria_global['funcoes'] if f.nome == nome_funcao][0]
 
 
 def verifica_existencia_id(ID):
