4f4dc0fa46367d949365761261bbba8ad55e5f58
João Pedro Gomes Carvalho <j.pedrogomes314@gmail.com>
2023-03-08 08:48:34 -0300

    Checagem de número e tipos dos parâmetros das funções

--- trabalhoFinalMyListener.py
+++ trabalhoFinalMyListener.py
@@ -29,7 +29,8 @@ tipo_declaracao = ''
 
 
 class Funcao():
-    ####### Tá faltando verificar a quantidade e o tipo dos parâmetros quando a função for chamada ########
+    ####### Tá faltando verificar a quantidade e o tipo dos parâmetros quando a função for chamada
+    ####### Feito 08/03/2023
 
     def __init__(self, nome, tipo_retorno, parametros:dict):
         self.nome = nome
@@ -37,23 +38,56 @@ class Funcao():
         self.variaveis = self.parametros
         self.tipo_retorno = tipo_retorno
 
-    def verifica_tipo_retorno(t):
+    def verifica_tipo_retorno(self, t):
         '''
             Verifica se o retorno utilizado é o tipo especificado quando a função foi instanciada.
         '''
 
         tipo = type(t)
 
-        if tipo_retorno == 'real' and tipo is float:
+        if self.tipo_retorno == 'real' and tipo is float:
             return True
-        elif tipo_retorno == 'int' and tipo is int:
+        elif self.tipo_retorno == 'int' and tipo is int:
             return True
-        elif tipo_retorno == 'bool' and tipo is bool:
+        elif self.tipo_retorno == 'bool' and tipo is bool:
             return True
-        elif tipo_retorno == 'String' and tipo is str:
+        elif self.tipo_retorno == 'String' and tipo is str:
             return True
 
         return False
+
+    """
+        Verifica se a função recebeu o número e o tipo correto de parâmetros
+    """
+    def verifica_numero_e_tipo_parametros(self, parametros:dict):
+        """
+        Número de parâmetros
+        """
+
+        if len(self.parametros) == len(parametros):
+            return True
+        else:
+            return False
+
+        """
+        Tipo dos parâmetros
+        Necessário transformar o dicionário em uma lista temporariamente para que os itens possam ser indexados
+        """
+
+        lista_parametros_passados = list(parametros.values())
+        lista_parametros = list(self.parametros.values())
+
+        for i in range(len(lista_parametros)):
+            if type(lista_parametros[i]) is not type(lista_parametros_passados[i]):
+                return False
+
+        return True
+
+
+
+
+
+
 
 
 class MyListener(ParseTreeListener):
